3fc6aad5afbefb88f457dad2f019b1f01e5e4cf0
Biki <Bikramjit.deka@gmail.com>
2020-10-13 16:46:55 +0000

    Added a comment line in the code

--- mainSummary.java
+++ mainSummary.java
@@ -1,7 +1,8 @@
 public class mainSummary {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-   System.out.println("Welcome to GIT");
+      System.out.println("Hello, World"); 
+    // Prints "Hello, World" in the terminal window.
+      System.out.println("Welcome to GIT");
    }
 }
