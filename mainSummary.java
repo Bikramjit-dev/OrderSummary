ac52305cf83c772811def739f491ba8d88c01e4b
Biki <Bikramjit.deka@gmail.com>
2020-10-14 01:36:28 +0000

    added new code for feature/linkedIn branch

--- mainSummary.java
+++ mainSummary.java
@@ -1,8 +1,8 @@
 public class mainSummary {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World"); 
-    // Prints "Hello, World" in the terminal window.
-      System.out.println("Welcome to GIT");
+      System.out.println("Hello, World");
+   System.out.println("Welcome to GIT");
+   System.out.println("code added part of my feature branchT")
    }
 }
